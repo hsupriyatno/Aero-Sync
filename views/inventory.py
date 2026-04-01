40ee8b719918d63698cee0ae2fa370ac9a3a6ad7
hsupriyatno <hsupriyatno@yahoo.com>
2026-04-01 08:50:37 +0700

    Update inventory.py

--- views/inventory.py
+++ views/inventory.py
@@ -3,7 +3,7 @@ from database import create_connection
 import datetime
 import pandas as pd
 import io
-from fpdf2 import FPDF
+from fpdf import FPDF
 # --- FUNGSI GENERATE NOMOR GRN ---
 def generate_grn_number():
     conn = create_connection()
